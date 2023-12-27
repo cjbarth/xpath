d20f16cc672f21a06a6f430798d6cb2aef774a67
Chris Barth <chrisjbarth@hotmail.com>
2023-12-27 04:52:50 -0600

    wip

--- xpath.d.ts
+++ xpath.d.ts
@@ -1,9 +1,9 @@
 /// <reference lib="dom" />
 
-export type SelectedValue = Node | string | number | boolean | null;
+export type ScalarValue = string | number | boolean;
 
-export type SelectReturnType = Array<Node> | SelectedValue;
-export type SelectSingleReturnType = SelectedValue;
+export type SelectReturnType = Array<Node> | ScalarValue;
+export type SelectSingleReturnType = ScalarValue | Node | undefined;
 
 export interface XPathSelect {
     (expression: string, node: Node): SelectReturnType;
@@ -26,9 +26,9 @@ export function select1(expression: string, node: Node): SelectSingleReturnType;
 /**
  * Evaluate an XPath expression against a DOM node using a given namespace resolver.
  */
-export function selectWithResolver(expression: string, node: Node, resolver?: XPathNSResolver | null): SelectReturnType;
-export function selectWithResolver(expression: string, node: Node, resolver: XPathNSResolver | null, single: false): SelectReturnType;
-export function selectWithResolver(expression: string, node: Node, resolver: XPathNSResolver | null, single: true): SelectSingleReturnType;
+export function selectWithResolver(expression: string, node: Node, resolver?: NonNullable<XPathNSResolver>): SelectReturnType;
+export function selectWithResolver(expression: string, node: Node, resolver: NonNullable<XPathNSResolver>, single: false): SelectReturnType;
+export function selectWithResolver(expression: string, node: Node, resolver: NonNullable<XPathNSResolver>, single: true): SelectSingleReturnType;
 
 /**
  * Creates a `select` function that uses the given namespace prefix to URI mappings when evaluating queries.
@@ -38,14 +38,14 @@ export function selectWithResolver(expression: string, node: Node, resolver: XPa
 export function useNamespaces(namespaceMap: Record<string, string>): XPathSelect;
 
 // Type guards to narrow down the type of the selected type of a returned Node object
-export function isNodeLike(value: SelectedValue): value is Node;
+export function isNodeLike(value: ScalarValue): value is Node;
 export function isArrayOfNodes(value: SelectReturnType): value is Node[];
-export function isElement(value: SelectedValue): value is Element;
-export function isAttribute(value: SelectedValue): value is Attr;
-export function isTextNode(value: SelectedValue): value is Text;
-export function isCDATASection(value: SelectedValue): value is CDATASection;
-export function isProcessingInstruction(value: SelectedValue): value is ProcessingInstruction;
-export function isComment(value: SelectedValue): value is Comment;
-export function isDocumentNode(value: SelectedValue): value is Document;
-export function isDocumentTypeNode(value: SelectedValue): value is DocumentType;
-export function isDocumentFragment(value: SelectedValue): value is DocumentFragment;
+export function isElement(value: ScalarValue): value is Element;
+export function isAttribute(value: ScalarValue): value is Attr;
+export function isTextNode(value: ScalarValue): value is Text;
+export function isCDATASection(value: ScalarValue): value is CDATASection;
+export function isProcessingInstruction(value: ScalarValue): value is ProcessingInstruction;
+export function isComment(value: ScalarValue): value is Comment;
+export function isDocumentNode(value: ScalarValue): value is Document;
+export function isDocumentTypeNode(value: ScalarValue): value is DocumentType;
+export function isDocumentFragment(value: ScalarValue): value is DocumentFragment;
